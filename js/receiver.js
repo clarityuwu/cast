dcb5b3eda88f75cdb2bdfb3956ca01cf0bf322c6
Raphael Boullay <raphael.boullay@gmail.com>
2024-04-20 22:35:57 +0200

    Update receiver.js

--- js/receiver.js
+++ js/receiver.js
@@ -4,10 +4,14 @@ const playerManager = context.getPlayerManager();
 // Set up the playback configuration
 const playbackConfig = new cast.framework.PlaybackConfig();
 playbackConfig.manifestRequestHandler = requestInfo => {
+    const url = new URL(requestInfo.url);
     requestInfo.headers['Referer'] = 'https://vidmoly.to';
+    requestInfo.headers['Host'] = url.host;
 };
 playbackConfig.segmentRequestHandler = requestInfo => {
+    const url = new URL(requestInfo.url);
     requestInfo.headers['Referer'] = 'https://vidmoly.to';
+    requestInfo.headers['Host'] = url.host;
 };
 
 context.start({ playbackConfig });
