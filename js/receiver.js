02135f9bbe47c8d0f528af3351d38a4628cb66e0
Raphael Boullay <raphael.boullay@gmail.com>
2024-04-20 22:15:21 +0200

    Create receiver.js

---
+++ js/receiver.js
@@ -0,0 +1,10 @@
+const context = cast.framework.CastReceiverContext.getInstance();
+const playerManager = context.getPlayerManager();
+
+// Set up the playback configuration
+const playbackConfig = new cast.framework.PlaybackConfig();
+playbackConfig.manifestRequestHandler = requestInfo => {
+    requestInfo.headers['Referer'] = 'https://vidmoly.to';
+};
+
+context.start({ playbackConfig });
